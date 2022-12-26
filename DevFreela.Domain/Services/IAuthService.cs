de91db66a41d4b1e8fca4d05e796e0e12804f8ae
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-25 23:22:41 -0300

    feature: implements authservice

---
+++ DevFreela.Domain/Services/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DevFreela.Domain.Services
+{
+	public interface IAuthService
+	{
+	}
+}
+
---
+++ DevFreela.Infrastructure/AuthService/AuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DevFreela.Infrastructure.AuthService
+{
+	public class AuthService
+	{
+		public AuthService()
+		{
+		}
+	}
+}
+
