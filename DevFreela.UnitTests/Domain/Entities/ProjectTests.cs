c49b642187707ec799cf394b59db3573fbc19c3d
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-12-27 23:32:09 -0300

    feature: add tests

---
+++ DevFreela.UnitTests/Domain/Entities/ProjectTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DevFreela.Domain.Entities;
+using DevFreela.Domain.Enums;
+
+namespace DevFreela.UnitTests.Domain.Entities
+{
+	public class ProjectTests
+	{
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Test Name", "Test Description", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
+
---
+++ DevFreela.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace DevFreela.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
